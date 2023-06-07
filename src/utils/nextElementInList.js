debf2040972161dd5a6cebdd61fffa7ee1645fcc
makinikayira <makinikayira@hollandandbarrett.com>
2023-06-07 19:37:42 +0100

    Redo - Reset

--- src/utils/nextElementInList.js
+++
@@ -1,8 +0,0 @@
-const nextElementInList = (list, currentElement) => {
-  const currentElementIndex = list.indexOf(currentElement)
-  const nextElementIndex = (currentElementIndex + 1) % list.length
-  const nextElement = list[nextElementIndex]
-  return nextElement
-}
-
-export default nextElementInList
