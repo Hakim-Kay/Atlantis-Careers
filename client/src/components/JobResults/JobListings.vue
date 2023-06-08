842925d705ae05bece7d76551993c9d2330f5817
makinikayira <makinikayira@hollandandbarrett.com>
2023-06-08 02:18:24 +0100

    project update

--- client/src/components/JobResults/JobListings.vue
+++ client/src/components/JobResults/JobListings.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
 
 import JobListing from '@/components/JobResults/JobListing.vue'
 
@@ -20,12 +20,12 @@ export default {
     return {
       jobs: []
     }
-  },
+  }
 
   // mounted is a lifecycle hook that runs after the component is mounted
-  async mounted() {
-    const response = await axios.get('http://localhost:3000/jobs')
-    this.jobs = response.data
-  }
+  // async mounted() {
+  //   const response = await axios.get('http://localhost:3000/jobs')
+  //   this.jobs = response.data
+  // }
 }
 </script>
